ced9e9874c802e5ee0f24d61978f058c9c9a9072
Ahmed Saleh Harby <109473873+A-Harby@users.noreply.github.com>
2024-01-30 12:40:02 +0200

    Update test_schema.py

--- grid-proxy/tests/scheme_test/test_schema.py
+++ grid-proxy/tests/scheme_test/test_schema.py
@@ -11,7 +11,7 @@ if network == 'dev':
 schema = schemathesis.from_path("docs/swagger.json", base_url = url)
 
 
-@pytest.mark.parametrize("check", [not_a_server_error, status_code_conformance, content_type_conformance, response_schema_conformance, response_headers_conformance])
+@pytest.mark.parametrize("check", [not_a_server_error])
 @schema.parametrize()
 def test_api(case, check):
     response = case.call()
